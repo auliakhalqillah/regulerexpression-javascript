039fef38e203b6699a243ebc2d8d50ab7176f56b
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2020-05-31 16:10:49 +0700

    Create regulerexpression.js

---
+++ regulerexpression.js
@@ -0,0 +1,26 @@
+// Using regex
+let input = 'abcdef123G';
+var num = /[\d+]/g;
+let lowercase = /[a-z]/g;
+let uppercase = /[A-Z]/g;
+var lengthinput = input.length;
+var min = 8;
+console.log("Length Password:",lengthinput);
+
+checkpassnum = input.match(num);
+checkpasslower = input.match(lowercase);
+checkpassupper = input.match(uppercase);
+
+console.log('CheckPassNum:', checkpassnum);
+console.log('CheckPassLower:', checkpasslower);
+console.log('CheckPassUpper:', checkpassupper);
+
+if (lengthinput < min){
+    console.log("Error: Password is to weak. At least has minimum 8 characters");
+} else if (checkpassnum == null || checkpassnum.length < 3) {
+    console.log("Error: Password at least has 3 digits number");
+} else if (checkpassupper == null) {
+    console.log("Error: Password at leas has 1 uppercase");
+} else {
+    console.log("Success!!!:", input);
+}
